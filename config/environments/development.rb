80272307815c6a06303b1e8bab9520469c70b3f5
Winston Pemberton <winstonpemberton@gmail.com>
2020-07-30 21:55:04 +0000

    Automatically backed up by Learn

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  config.web_console.whitelisted_ips = '<IP address here>'
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -53,6 +55,3 @@ Rails.application.configure do
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
 
-class Application < Rails::Application
-  config.web_console.whitelisted_ips = '<IP address here>'
-end
